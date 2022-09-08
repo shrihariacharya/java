cbb9e3bf1e1a84551e808a3f908722182b640a27
shrihariacharya <113082126+shrihariacharya@users.noreply.github.com>
2022-09-08 12:42:01 +0530

    Add files via upload

---
+++ greatest.java
@@ -0,0 +1,23 @@
+package labtest01;
+
+public class greatest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	     int x=10, y=20, z=30;
+	        
+	        if(x > y && x > z)
+	        {
+	            System.out.println("Largest number is:"+x);
+	        }
+	        else if(y > z)
+	        {
+	            System.out.println("Largest number is:"+y);
+	        }
+	        else
+	        {
+	            System.out.println("Largest number is:"+z);
+	        }
+	}
+
+}
